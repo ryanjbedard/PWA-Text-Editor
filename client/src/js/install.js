aabb3413c3796e01454f4841fb45bf8dca7e4578
ryanjbedard <143044952+ryanjbedard@users.noreply.github.com>
2024-03-12 19:58:42 -0700

    Add event handler for 'beforeinstallprompt'

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,8 +1,16 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
+let deferredPrompt;
+
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {});
+     // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  butInstall.style.visibility = 'visible';
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {});
